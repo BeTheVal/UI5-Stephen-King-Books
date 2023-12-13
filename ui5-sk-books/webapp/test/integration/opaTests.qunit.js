7076be37e7cd917a64188d1663c8491249c95fa8
Betheval <miguelgutp@gmail.com>
2023-12-13 20:47:27 +0100

    Initial commit

---
+++ ui5-sk-books/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["betheval/dev/ui5skbooks/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ ui5-sk-books/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"bethevaldev/ui5-sk-books/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
